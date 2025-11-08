d883583bd329bbee648c78ab1672595ad6d06810
Timofeev1985 <timon_1985_@mail.ru>
2025-11-08 13:42:29 +0300

    fix: при открытии одного элемента, другие закрываются

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// const contents = document.querySelectorAll(".program-line__content");
+
+// contents.forEach((elem) => {
+//   const title = elem.querySelector(".program-line__title");
+//   const descr = elem.querySelector(".program-line__descr");
+
+//   title.addEventListener("click", () => {
+//     descr.classList.toggle("active");
+//   });
+// });
 const contents = document.querySelectorAll(".program-line__content");
 
 contents.forEach((elem) => {
@@ -5,6 +15,12 @@ contents.forEach((elem) => {
   const descr = elem.querySelector(".program-line__descr");
 
   title.addEventListener("click", () => {
+    contents.forEach((otherElem) => {
+      if (otherElem !== elem) {
+        const otherDescr = otherElem.querySelector(".program-line__descr");
+        otherDescr.classList.remove("active");
+      }
+    });
     descr.classList.toggle("active");
   });
 });
